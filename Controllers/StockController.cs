e84ed25eddc1096a26c188ad9cd6503d7493da1b
Pahan Chathuranga <pahanchathuranga123@gmail.com>
2024-10-18 20:28:39 +0530

    Get stocks and Get stocks/{id}

---
+++ Controllers/StockController.cs
@@ -0,0 +1,38 @@
+﻿using api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        public StockController(ApplicationDBContext context) 
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stocks.ToList();
+
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id) // model binding
+        {
+            var stock = _context.Stocks.Find(id); // FirstOrDefault also can be utilised.
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock);
+        }
+    }
+}
